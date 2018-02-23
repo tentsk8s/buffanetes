6bd88f65db9d7aca5eb7d2703d71a6076f4a7c51
Aaron Schlesinger <arschles@gmail.com>
2018-02-22 18:51:11 -0800

    adding init command

--- cmd/cli/init_cmd.go
+++ cmd/cli/init_cmd.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,7 +13,15 @@ func initCmd() *cobra.Command {
 		Use:   "init",
 		Short: "Initialize your Buffalo app to work with Buffanetes",
 		RunE: func(cmd *cobra.Command, args []string) error {
-
+			if _, err := os.Stat(".buffanetes"); err == nil {
+				return fmt.Errorf("the .buffanetes directory already exists, bailing out")
+			}
+			curDir, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+			appName := filepath.Base(curDir)
+			logger.Printf("initializing buffanetes app %s", appName)
 			return nil
 		},
 	}
